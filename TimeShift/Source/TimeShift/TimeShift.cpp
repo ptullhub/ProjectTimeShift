6882e906521b25b113512abedb9aaebd8e2acd23
ptullhub <111908049+ptullhub@users.noreply.github.com>
2025-03-26 02:11:38 -0400

    Revert "projectregeneration"
    This reverts commit d9b2cc499d99b74cbf31b5dfb0d29c6d3ba659fa.

--- TimeShift/Source/TimeShift/TimeShift.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TimeShift.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TimeShift, "TimeShift" );
